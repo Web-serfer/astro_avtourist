8ce605194d135bcd45a14e65493e41b0229f30b9
RediBedi <redibedi2019@gmail.com>
2025-03-16 19:12:02 +0500

    16/03/25

---
+++ src/constants.ts
@@ -0,0 +1,9 @@
+// данные портала
+export const BLOG_TITLE = "Блог Автоюриста";
+export const SITE_DESCRIPTION = "Сайт посвящен защите прав автомобилистов";
+
+// количество статей на странице
+export const ARTICLES_PER_PAGE = 3;
+
+// текущий год
+export const CURRENT_YEAR = new Date().getFullYear();
--- src/consts.ts
+++
@@ -1,4 +0,0 @@
-export const BLOG_TITLE = 'Блог Автоюриста';
-export const SITE_DESCRIPTION = 'Сайт посвящен защите прав автомобилистов';
-
-export const ARTICLES_PER_PAGE = 3;

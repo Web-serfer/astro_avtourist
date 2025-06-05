be8339aff188a7cbc51e4e6990fac39dfba766bf
RediBedi <redibedi2019@gmail.com>
2025-06-05 17:28:32 +0500

    05/06/25

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,12 +5,11 @@ import mdx from "@astrojs/mdx";
 import icon from "astro-icon";
 import vercel from "@astrojs/vercel";
 import tailwind from "@astrojs/tailwind";
-import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://avtourist-surgut.ru",
-  integrations: [mdx(), tailwind(), icon(), partytown()],
+  integrations: [mdx(), tailwind(), icon()],
   markdown: {
     syntaxHighlight: "shiki",
   },
